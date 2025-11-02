de77119c4b5bc8ea813bb9fe2d6a9270089d1f44
Milenium666 <whiteangel64rus@gmail.com>
2025-11-02 17:43:39 +0300

    feat(form):implemented component app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from 'vue';
+import FormGenerator from './components/FormGenerator.vue'
+
+const formSchema = {
+  fields: [
+    { type: 'text', label: 'Имя', model: 'name', required: true },
+    { type: 'email', label: 'Email', model: 'email', required: true },
+    { type: 'password', label: 'Пароль', model: 'password', required: true, minLength: 6 },
+    { type: 'select', label: 'Роль', model: 'role', options: ['Админ', 'Пользователь'], required: true },
+    { type: 'checkbox', label: 'Согласен с условиями', model: 'terms', required: true }
+  ]
+};
+
+const formData = reactive({
+  name: '',
+  email: '',
+  password: '',
+  role: '',
+  terms: false
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+
+  <div style="max-width: 500px; margin: 40px auto; padding: 20px;">
+    <h2>Автоформа</h2>
+    <FormGenerator :schema="formSchema" v-model="formData" />
+    <pre>{{ JSON.stringify(formData, null, 2) }}</pre>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
